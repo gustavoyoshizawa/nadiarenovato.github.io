e3c6570becd24f47ae9e12d936e94aa114ab801d
Gustavo Yoshizawa <gustavoyoshizawa89@gmail.com>
2025-01-17 05:37:23 -0300

    ajustando js

--- js/modules/tabnav.js
+++ js/modules/tabnav.js
@@ -3,39 +3,31 @@ export default function initTabNav() {
   const menuList = document.querySelector('[data-menu="list"]');
   const eventos = ["click", "touchstart"];
 
-  function openMenu(event) {
-    event.stopPropagation();
-    if (menuButton.classList.contains("ativo")) {
-      closeMenu();
-    } else {
-      menuButton.classList.add("ativo");
-      menuList.classList.add("ativo");
-      outsideClick(menuList, eventos, closeMenu);
-    }
-  }
+  function openMenu() {
+    menuButton.classList.add("ativo");
+    menuList.classList.add("ativo");
+    outsideClick(this, ["touchstart", "click"], () => {
+      menuButton.classList.remove("ativo");
+      menuList.classList.remove("ativo");
+    });
 
-  function closeMenu() {
-    menuButton.classList.remove("ativo");
-    menuList.classList.remove("ativo");
-  }
-
-  function outsideClick(element, events, callback) {
-    const html = document.documentElement;
-    const outside = "data-outside";
-    if (!element.hasAttribute(outside)) {
-      events.forEach((userEvent) => {
-        setTimeout(() => html.addEventListener(userEvent, handleOutsideClick));
-      });
-      element.setAttribute(outside, "");
-    }
-
-    function handleOutsideClick(event) {
-      if (!element.contains(event.target)) {
-        element.removeAttribute(outside);
+    function outsideClick(element, events, callback) {
+      const html = document.documentElement;
+      const outside = "data-outside";
+      if (!element.hasAttribute(outside)) {
         events.forEach((userEvent) => {
-          html.removeEventListener(userEvent, handleOutsideClick);
+          html.addEventListener(userEvent, handleOutsideClick);
         });
-        callback();
+        element.setAttribute(outside, "");
+      }
+      function handleOutsideClick(event) {
+        if (!element.contains(event.target)) {
+          element.removeAttribute(outside);
+          events.forEach((userEvent) => {
+            html.removeEventListener(userEvent, handleOutsideClick);
+          });
+          callback();
+        }
       }
     }
   }
