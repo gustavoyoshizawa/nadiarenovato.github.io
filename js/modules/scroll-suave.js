7a69fb293c2ab3105505209a6146505d62ca7998
Gustavo Yoshizawa <gustavoyoshizawa89@gmail.com>
2025-01-17 05:53:09 -0300

    ajustando bug scroll

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -2,7 +2,7 @@ export default function scrollSuave() {
   const sections = document.querySelectorAll(".js-scroll");
   sections[0].classList.add("ativo");
   if (sections.length) {
-    const windowMetade = window.innerHeight * 0.9;
+    const windowMetade = window.innerHeight * 0.93;
 
     function animarScroll() {
       sections.forEach((section) => {
